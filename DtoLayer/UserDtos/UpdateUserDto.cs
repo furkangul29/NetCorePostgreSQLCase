7615de83d95e82ff915b4faee02d1048932660a2
furkangul29 <gulfurkan@outlook.com.tr>
2024-10-21 20:14:26 +0300

    Database connection was created and migration process was done. Sample data set entry was made via PostgreSql. DtoLayer was created. DtoLayer and EntityLayer were mapped using AutoMapper. Api service was written for Customers, CustomerFilters and User

---
+++ DtoLayer/UserDtos/UpdateUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DtoLayer.UserDtos
+{
+    public class UpdateUserDto
+    {
+        //public int Id { get; set; } 
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
