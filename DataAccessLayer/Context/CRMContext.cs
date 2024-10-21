4ea13238517076347be480277ead4677b8520489
furkangul29 <gulfurkan@outlook.com.tr>
2024-10-21 12:06:22 +0300

    Add project files.

---
+++ DataAccessLayer/Context/CRMContext.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Context
+{
+    public class CRMContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public CRMContext(DbContextOptions<CRMContext> options) : base(options) { }
+    }
+    
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly CRMContext _crmContext;
+
+        public GenericRepository(CRMContext crmContext)
+        {
+            _crmContext = crmContext;
+        }
+
+        public void Add(T entity)
+        {
+            _crmContext.Add(entity);
+            _crmContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _crmContext.Remove(entity);
+            _crmContext.SaveChanges();
+        }
+
+
+        public T GetbyID(int ID)
+        {
+            var values= _crmContext.Set<T>().Find(ID);
+            return values;
+        }
+
+        public List<T> GetListAll()
+        {
+           var values= _crmContext.Set<T>().ToList();
+            return values;
+        }
+
+        public void Update(T entity)
+        {
+            _crmContext.Update(entity);
+            _crmContext.SaveChanges();
+        }
+    }
+}
